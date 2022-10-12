5a1ccf9b55587478667503fe642fc15afad6e4a8
And4Web <anands2807@gmail.com>
2022-10-12 20:43:47 +0530

    graphql small project is almost done with books and authors query created.

---
+++ data.js
@@ -0,0 +1,21 @@
+module.exports.authors = [
+  {id: 1, name: 'S. Tavaria'},
+  {id: 2, name: 'Vedvyas'},
+  {id: 3, name: 'Patanjali'},
+]
+
+module.exports.books = [
+  {id: 1, name: 'Purpose of Birth and Death', authorId: 1},
+  {id: 2, name: 'Education for attention', authorId: 1},
+  {id: 3, name: 'Mahabharat', authorId: 2},
+  {id: 4, name: 'Shrimad BhagvadPuran', authorId: 2},
+  {id: 5, name: 'Yogsutra', authorId: 3},
+  {id: 6, name: 'VyakaranShastra', authorId: 3},
+  {id: 7, name: 'Hathyog Pradipika', authorId: 4},
+  {id: 8, name: 'Upnishads', authorId: 2}
+]
+
+// module.exports = {
+//   authors, 
+//   books
+// }
--- server.js
+++ server.js
@@ -3,10 +3,69 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const {graphqlHTTP} = require('express-graphql');
-const {buildSchema} = require('graphql');
+const {GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList, GraphQLNonNull, GraphQLInt} = require('graphql');
+
+const {authors, books} = require('./data');
+
+const BookType = new GraphQLObjectType({
+  name: 'Books',
+  description: 'A single Book',
+  fields: () => ({
+    id: {type: GraphQLNonNull(GraphQLInt)},
+    name: {type: GraphQLNonNull(GraphQLString)},
+    authorId: {type: GraphQLNonNull(GraphQLInt)},
+    author: {
+      type: AuthorType,
+      resolve: (book) => {
+       return authors.find(author => author.id === book.authorId)
+      }
+    }
+  })
+})
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  description: 'Author of the current book',
+  fields: () => ({
+    id: {type: GraphQLNonNull(GraphQLInt)},
+    name: {type: GraphQLNonNull(GraphQLString)},    
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: (author) => {
+        return books.filter(book => book.authorId === author.id)
+      }
+    }
+  })
+})
+
+const rootQueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Root Query',
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: 'List of All Books',
+      resolve: () => books
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: 'List of All Authors',
+      resolve: () => authors
+    },
+    test: {
+      type: GraphQLString,
+      resolve: () => 'hello test'
+    }
+  })
+})
+
+const Schema = new GraphQLSchema({
+  query: rootQueryType
+})
 
 app.use('/graphql', graphqlHTTP({
-  // graphiql: true
+  schema: Schema,
+  graphiql: true
 }))
 
 app.get('/', (req, res) => {
